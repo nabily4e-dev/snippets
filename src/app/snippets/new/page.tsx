4601f08b85bb94d6fc6878ca08b3690285af3427
Nabil Al-Ashbat <nbl.dev21@gmail.com>
2023-12-17 00:23:42 +0300

    Video 26 - Adding Dynamic Paths Complete

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -25,31 +25,31 @@ export default function SnippetCreatePage() {
 
   return (
     <form action={createSnippet}>
-      <h3 className='font-bold m-3'>Create a Snippet</h3>
-      <div className='flex flex-col gap-4'>
-        <div className='flex gap-4'>
-          <label className='w-12' htmlFor='title'>
+      <h3 className="font-bold m-3">Create a Snippet</h3>
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="title">
             Title
           </label>
           <input
-            name='title'
-            className='border rounded p-2 w-full'
-            id='title'
+            name="title"
+            className="border rounded p-2 w-full"
+            id="title"
           />
         </div>
 
-        <div className='flex gap-4'>
-          <label className='w-12' htmlFor='code'>
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="code">
             Code
           </label>
           <textarea
-            name='code'
-            className='border rounded p-2 w-full'
-            id='code'
+            name="code"
+            className="border rounded p-2 w-full"
+            id="code"
           />
         </div>
 
-        <button type='submit' className='rounded p-2 bg-blue-200'>
+        <button type="submit" className="rounded p-2 bg-blue-200">
           Create
         </button>
       </div>
